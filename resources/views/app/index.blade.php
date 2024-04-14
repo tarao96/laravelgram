52619d4eb072516561949d0ecaa1033e794e1391
skeito <s.keito896@gmail.com>
2024-04-14 10:46:17 +0900

    add: 投稿一覧画面を仮で作成

--- resources/views/app/index.blade.php
+++ resources/views/app/index.blade.php
@@ -1,5 +1,20 @@
 @extends('layouts.app')
 @section('title', '投稿一覧画面')
 @section('content')
-<a href="{{ route('user.mypage') }}">マイページ</a>
+<a href="{{ route('user.mypage') }}" class="mypage-link">マイページ</a>
+<div class="posts">
+    @foreach ($posts as $post)
+    <div class="card">
+        <div class="card-img">
+            <img src="img/sample-img.jpeg" alt="サンプル画像">
+        </div>
+        <div class="card-body">
+            <div class="card-title">
+                <h3>{{ $post->title }}</h3>
+            </div>
+            {{ $post->body}}
+        </div>
+    </div>
+    @endforeach
+</div>
 @endsection
